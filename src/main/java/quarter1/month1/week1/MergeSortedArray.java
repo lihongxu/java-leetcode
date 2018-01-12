852a986fdb152849e487d6b5126c07f9b177c024
lihongxu <lihongxu@baidu.com>
2018-01-12 16:14:44 +0800

    1 week commit

---
+++ src/main/java/quarter1/month1/week1/MergeSortedArray.java
@@ -0,0 +1,47 @@
+package quarter1.month1.week1;
+
+/**
+ * User: lihongxu
+ * Date: 18/1/4
+ * Time: 17:28
+ * Comments https://leetcode.com/problems/merge-sorted-array/description/
+ */
+public class MergeSortedArray {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m -1 ;
+        int j = n -1 ;
+        int k = m + n - 1;
+
+        while (i >=0 && j >=0){
+            if (nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while (j>=0){
+            nums1[k--] = nums2[j--];
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[6];
+        nums1[0] = 2;
+        nums1[1] = 7;
+        nums1[2] = 8;
+
+        int[] nums2 = {1,9};
+
+        merge(nums1,3,nums2,2);
+
+        for (int i : nums1) {
+            System.out.println(i);
+        }
+
+    }
+}
